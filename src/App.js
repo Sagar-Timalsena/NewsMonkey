2eefaf7c7e91e569840e1aaf990823b1db336a90
SAAGAR <38255285+Sagar1555@users.noreply.github.com>
2022-03-26 09:48:37 +0545

    News App Api Call

---
+++ src/App.js
@@ -0,0 +1,18 @@
+//import logo from './logo.svg';
+import './App.css';
+
+import React, { Component } from 'react';
+import Navbar from './components/Navbar';
+
+export default class FileName extends Component {
+
+  render() {
+    return(
+      <div>
+
+      <Navbar />
+
+      </div>
+    )
+  }
+}
